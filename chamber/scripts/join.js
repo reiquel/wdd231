f96e9b523402e5d7db4ec28a873f33703268fbaf
reiquel <carelary92@gmail.com>
2025-03-29 10:42:50 -0400

    hdh

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -19,16 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-
-const myInfo = new URLSearchParams(window.location.search);
-
-document.querySelector('#results').innerHTML = `
-<p>Dear ${myInfo.get('first-name')} ${myInfo.get('last-name')},</p>
-<p>You have enrolled for the <strong>${myInfo.get('membership-level')}</strong> with our chamber of commerce on ${myInfo.get('date-and-time')}</p>
-<p>We are excited to have ${myInfo.get('business-name')} as part of our growing business community.</p>
-<p>Our team will review your application and contact you within 2 business days at ${myInfo.get('email')} or ${myInfo.get('cell-phone')}</p>
-`;
-
 });
 
 const hamburgerElement = document.querySelector('#hamburger-button');
