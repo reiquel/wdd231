0638234d8f8e384971126aea41d5861ab80f4f14
reiquel <carelary92@gmail.com>
2025-04-10 20:31:14 -0400

    gsgsg

---
+++ Project/scripts/clothing.js
@@ -0,0 +1,46 @@
+
+import { addToCart} from "./cart.js";
+
+
+export async function renderProducts(category) {
+    const productGrid = document.getElementById("clothing-container") 
+        if (!productGrid) return;
+    
+        try {
+            const response = await fetch("./clothing.json");
+            const products = await response.json();
+
+            const categoryProducts = products[category]  || 
+                                     Object.values(products).flat();
+            productGrid.innerHTML = categoryProducts.map(product =>`
+                <div class="product-card">
+                    <img src="images/${product.image}" alt="${product.name}" class="product-image">
+                    <div class="product-info">
+                         <h3 class="product-title">${product.name}</h3>
+                        <p class="product-price">$${product-price.toFixed(2)}</p>
+                        <button class="add-to-cart" data-id="${product.id}">
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+                `).join("");
+            document.querySelectorAll(".add-to-cart").forEach(button =>{
+                button.addEventListener("click", (e) => {
+                    const productId = parseInt(e.target.dataset.id);
+                    const product = Object.values(products).flat().find(p => p.id === productId);
+
+                    if(product) {
+                        if (addToCart(product)) {
+                            const card = e.target.closest(".product-card");
+                            card.style.transform = "scale(1.05)";
+                            setTimeout(() => {
+                                card.style.transform = "";
+                            },300)
+                        }
+                    }
+                })
+            })
+        } catch (error) {
+            productGrid.innerHTML = `<p>Error loading products. Please try again later.</p>`;
+        }
+}
---
+++ Project/scripts/main.js
@@ -0,0 +1,53 @@
+import { renderCartItems,setupCheckout, updateCart} from "./cart.js";
+import { renderProducts } from "./clothing.js";
+import { setupOrderConfirmation } from "./confirmation.js";
+
+document.addEventListener('DOMContentLoaded', () =>{
+    updateCart();
+
+    const sideNav = document.getElementById('sideNavbar');
+    const openNav = document.getElementById('openNav');
+    const closeNav = document.getElementById('closeNav');
+
+    if (openNav) {
+        openNav.addEventListener('click', () => {
+            sideNav.classList.add('active');
+            document.body.style.overflow = 'hidden';
+        })
+    }
+
+    if (closeNav) {
+        closeNav.addEventListener('click', () => {
+            sideNav.classList.remove('active');
+            document.body.style.overflow = '';
+        })
+    }
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.side-navbar') && 
+            !e.target.closest('.nav-toggle') &&
+            sideNav.classList.contains('active')) {
+          sideNav.classList.remove('active');
+          document.body.style.overflow = '';
+        }
+      });
+
+      const path = window.location.pathname.split('/').pop();
+
+      switch(path) {
+        case 'index.html':
+            renderProducts('men');
+            renderProducts('women');
+            renderProducts('accessories');
+            break;
+        case 'cart.html':
+            renderCartItems();
+            setupCheckout();
+            break;
+        case 'confirmation.html':
+            setupOrderConfirmation();
+            break;
+        default:
+            renderProducts();
+      }
+})
