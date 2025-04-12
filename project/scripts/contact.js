a66ab7de13a3af50dfa88e21d175b3e820bd1a46
reiquel <carelary92@gmail.com>
2025-04-12 09:36:22 -0400

    sfsg

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -1,8 +1,32 @@
-const partnershipForm = document.querySelector(".membership-form");
+document.addEventListener("DOMContentLoaded", () => {
+    const openButtons = document.querySelector('.open-button');
+    const closeButtons = document.querySelector('.close-button');
+
+    openButton.forEach(button => {
+        button.addEventListener("click", () => {
+            const modalId = button.getAttribute("data-modal");
+            const modal = document.getElementById(modalId);
+            if(modal) {
+                modal.showModal();
+            }
+        });
+        
+    });
+
+    closeButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            button.closest(".modal").close();
+        });
+    });
+
+    const partnershipForm = document.querySelector(".membership-form");
 if (partnershipForm) {
     partnershipForm.addEventListener("submit", (e) => {
         e.preventDefault();
         alert("Thank you for your partnership request! We'll get back to you soon.");
         partnershipForm.reset();
     });
-}
+   }
+});
+
+
