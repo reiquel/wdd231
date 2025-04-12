9c55b2c8b076e489dc79405b1826e40e2fba0137
reiquel <carelary92@gmail.com>
2025-04-12 00:23:32 -0400

    fafa

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -1,8 +1,8 @@
-const partnershipForm = document.getElementById(".membership-form");
+const partnershipForm = document.querySelector(".membership-form");
 if (partnershipForm) {
     partnershipForm.addEventListener("submit", (e) => {
         e.preventDefault();
         alert("Thank you for your partnership request! We'll get back to you soon.");
-        partnershipForm.requestFullscreen();
+        partnershipForm.reset();
     });
 }
